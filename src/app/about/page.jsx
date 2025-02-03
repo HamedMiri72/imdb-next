afc8010cf6b97d6642c2c97490eadd670cda9600
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-03 17:30:05 +0000

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>imdb clone is a movie and TV show database that lets users explore detailed filmographies, ratings, reviews, and trailers in one place. Whether you're searching for blockbuster hits, indie films, or upcoming releases, our platform provides comprehensive information on cast, crew, and behind-the-scenes insights. With personalized watchlists and real-time trending content, we aim to enhance your cinematic experience. Dive into the world of entertainment and discover your next favorite movie or show with imdb clone </p>
+    </div>
+  )
+}
