59bcdeda8990d994f5c588420660faed218c0e03
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-03 19:58:36 +0000

    fetch data

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Providers from "./Providers";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Providers>
             <Header/>
+            <Navbar/>
             {children}
           </Providers>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import Results from './components/results';
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+
+  const genre = searchParams.genre || 'fetchTrending'
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
+      language=en-US&page=1`
+  );
+  const data =  await response.json();
+  if(!response.ok){
+    throw new Error("failed to fetch")
+  }
+  const results = data.results; 
+  
+
   return (
-    <div></div>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
