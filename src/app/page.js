ebfe5b58b3432295d5e993496b7f3b8d6030e33d
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-03 20:53:49 +0000

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,14 @@ const API_KEY = process.env.API_KEY;
 export default async function Home({searchParams}) {
 
   const genre = searchParams.genre || 'fetchTrending'
+  
   const response = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
-      language=en-US&page=1`
+      language=en-US&page=1`,
+      {next: {revalidate: 10000}}
   );
+  
   const data =  await response.json();
   if(!response.ok){
     throw new Error("failed to fetch")
